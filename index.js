0c6871b499ab586457b5fd664e80a48865896bb5
Ravi Chauhan <138319480+hamravichauhan@users.noreply.github.com>
2024-03-24 12:30:22 +0530

    "Update index.js and style.css: add smooth scrolling, update header and contact info styles, and adjust font size"

--- index.js
+++ index.js
@@ -81,3 +81,15 @@ function redirectToWhatsApp(carName = '') {
   
   adjustFontSize();
   startSlideshow();
+
+
+  // Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
